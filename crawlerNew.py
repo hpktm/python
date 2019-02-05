1332a563c6b49b06c4d1a66e84356cceb100d238
hpktm <hitesh.pant94@gmail.com>
2019-02-05 22:34:30 +0530

    includeing header

--- crawlerNew.py
+++ crawlerNew.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+from string import ascii_lowercase
+from csv import writer  
+
+
+#gender of the child
+gender=["boy","girl"]
+
+#base url of the site to be scrap
+baseUrl="https://www.babynamesdirect.com";
+
+#response for the request form the basr url
+response=requests.get(baseUrl);
+
+#html parser for the url soup=>object for the bs4 BeautifulSoup
+soup=BeautifulSoup(response.text,'html.parser')
+
+#loocking for the specific details
+options=soup.find_all('option')
+
+typeName=[]
